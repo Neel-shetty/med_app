61896882af58bb323327765c3a21a280cc3e05fe
Neel Narayan Shetty <neel.shetty_int@acko.tech>
2026-01-14 03:42:36 +0530

    db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:TqJv8eD9dg49EydYXo4oCTFF@172.21.23.181:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
